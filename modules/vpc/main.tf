746d846a0de1534ced963ee1f85d602be255de16
inobu <inohkunobuya@gmail.com>
2021-02-04 23:00:51 +0900

    public subnetを追加

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -8,6 +8,21 @@ resource "aws_vpc" "example_vpc" {
   }
 }
 
+resource "aws_subnet" "public_0" {
+  vpc_id                  = aws_vpc.example_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.example_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = true
+}
+
+
 resource "aws_subnet" "private_0" {
   vpc_id                  = aws_vpc.example_vpc.id
   cidr_block              = "10.0.65.0/24"
--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -6,6 +6,14 @@ output "aws_subnet_private_1" {
   value = aws_subnet.private_1
 }
 
+output "aws_subnet_public_0" {
+  value = aws_subnet.public_0
+}
+
+output "aws_subnet_public_1" {
+  value = aws_subnet.public_1
+}
+
 output "cidr_block" {
   value = aws_vpc.example_vpc.cidr_block
 }
